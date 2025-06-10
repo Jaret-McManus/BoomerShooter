3676329d3348b8919255c38d44a82395009edabb
CharleyBirb <charley.birb@gmail.com>
2025-06-10 09:28:10 -0700

    Test Layout model and textures updated

--- scripts/test_enemy.gd
+++ scripts/test_enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 const SPEED := 5.0
 const JUMP_VELOCITY := 4.5
-const ROTATION_SPEED := 185.0
+const ROTATION_SPEED := 110.0
 
 @export var ANIMATION_PLAYER : AnimationPlayer
 @export var SPRITE : Sprite3D
